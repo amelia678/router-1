2d15c8599309eaa08b242565a91d2b1aba8c9ed1
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-11 09:35:18 -0500

    dynamically adds cat toys, adds dog names

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
 
 import {
   BrowserRouter as Router, //watches for changes to URL
   Route,    //renders components
-  Link      // changes URL so router sees it
+  // Link      // changes URL so router sees it
 } from 'react-router-dom';
 
+import NavBar from './NavBar';
 import Cats from './Cats';
 import About from './About';
 import Home from './Home';
@@ -37,24 +37,41 @@ class App extends Component {
           'chipmunks',
           'blood'
         ]
+      },
+
+      dogToys: {
+        'Riley': [
+          'bones',
+          'cat toys'
+        ],
+
+        'Toby': [
+          'lamby',
+          'bone'
+        ],
+
+        'Peaches' :[
+          'heater',
+          'porkchops'
+        ]
       }
     }
   }
   render() {
     return (
       <Router>
-      <div className="App">
+      {/* <div className="App"> */}
+      <div>
       <NavBar />
       <Route path="/" exact component= {Home} />
-      <Route path="/about" exact component= {About} />
-      {/* <Route path="/cats" exact component= {Cats} />
-      <Route path="/cats/:catName" component = {OneCat} /> */}
+      <Route path="/about"  component= {About} />
       <Route path="/cats" render={(props) => {
-        return <Cats catList={Object.keys(this.state.catToys)}{...props} />
+        return <Cats catList={Object.keys(this.state.catToys)} {...props} />
       }} />
-      <Route path="/cats:catName" render={(props) => {
-        return <OneCat toys={this.state.catToys}{...props} />
+      <Route path="/cats/:catName" render={(props) => {
+        return <OneCat toys={this.state.catToys} {...props} />
       }} />
+      <Route path="/dogs" component={Dogs} />
       </div>
       </Router>
     );
